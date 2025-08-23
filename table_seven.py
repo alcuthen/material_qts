aa1bc8178abb13d3efa609a0934bedd747bf2371
alcuthen <alcuthen@gmail.com>
2025-08-23 10:29:26 -0300

    corrijido

--- table_seven.py
+++ table_seven.py
@@ -2,49 +2,43 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from credential import url, pwd, log_user  # biblioteca criada
 
-from credential import url, pwd, log_user #biblioteca criada.
-
-# Criando um função chamada acess()
-
-browser: object = webdriver.Chrome() 
-browser.get(url)
-
+# Função principal de acesso
 def acess(browser):
-       
-    title_page: str = browser.title
+    browser.get(url)
+    title_page = browser.title
 
-    if url == 'https://www.saucedemo.com' and title_page == 'Swang Labs':
+    if url == 'https://www.saucedemo.com' and title_page == 'Swag Labs':
         print("Acesso correto")
 
-        #browser.find_element(By.id, "user-name") ou por XPATH
-        login_user: object = browser.find_element(By.XPATH, "//input[@id='user-name']")
-        login_password: object = browser.find_element(By.XPATH, "//input[@id='password']")
-        login_btn_log: object = browser.find_element(By.XPATH, "//input[@value='Login']")
+        # Mapeamento dos campos
+        login_user = browser.find_element(By.XPATH, "//input[@id='user-name']")
+        login_password = browser.find_element(By.XPATH, "//input[@id='password']")
+        login_btn_log = browser.find_element(By.XPATH, "//input[@value='Login']")
 
-        #Enviando dados de log, senha e submissão para os campos mapeados 
+        # Enviando dados
         login_user.send_keys(log_user)
         login_password.send_keys(pwd)
 
-        time.sleep(10)
+        time.sleep(2)
 
         login_btn_log.click()
 
-        title_product: str = browser.find_element(By.XPATH, "//span[@data-test='title']")
+        time.sleep(2)
 
+        title_product = browser.find_element(By.XPATH, "//span[@data-test='title']").text
 
-        #assert recebe True
-        #Format
-        # assert COMPARAÇÃO, [ OPÇÃO DE LANÇAMENTO DE MENSAGEM ]
-        assert browser.title == "https://www.saucedemo.com/inventory.html", "Página interna não acessada"
-        assert title_product == "Products", "Não encontrei o título recomendado: {title_product}."
+        # Asserts
+        assert browser.current_url == "https://www.saucedemo.com/inventory.html", "Página interna não acessada"
+        assert title_product == "Products", f"Não encontrei o título recomendado: {title_product}."
 
-try:
-    if __name__ == "__main__":
+# Execução principal
+if __name__ == "__main__":
+    browser = webdriver.Chrome()
+    try:
         acess(browser)
-
-except:
-    print("O Rauni não arrumou a rede ainda")
-
-finally:
-    browser.quit()
+    except Exception as e:
+        print(f"O Rauni não arrumou a rede ainda: {e}")
+    finally:
+        browser.quit()
